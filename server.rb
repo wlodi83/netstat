acacd991f918cf21fa4e8867ceb276d4d06b4ecf
Łukasz Włodarczyk <wlodi@jamaicans.com>
2012-01-27 12:48:02 +0100

    Better solution for displaying

--- server.rb
+++ server.rb
@@ -5,52 +5,20 @@ class Netstat
 
   def self.open
     #check port 3489
-    netstat_3489 = IO.popen("netstat -an | grep 3489")
+    netstat_3489 = IO.popen("netstat -an | grep ::3489")
     #check port 3589
-    netstat_3589 = IO.popen("netstat -an | grep 3589")
+    netstat_3589 = IO.popen("netstat -an | grep ::3589")
     #check port 3689
-    netstat_3689 = IO.popen("netstat -an | grep 3689")
+    netstat_3689 = IO.popen("netstat -an | grep ::3689")
     #check port 3789
-    netstat_3789 = IO.popen("netstat -an | grep 3789")
-    #checking results
-    if result_1 = netstat_3489.gets && result_2 = netstat_3589.gets && result_3 = netstat_3689.gets && result_4 = netstat_3789.gets
-      return result_1, result_2, result_3, result_4
-    elsif result_1 = netstat_3489.gets && result_2 = netstat_3589.gets && result_3 = netstat_3689.gets
-      return result_1, result_2, result_3, "PORT 3789 is FREE"
-    elsif result_1 = netstat_3489.gets && result_2 = netstat_3589.gets && result_4 = netstat_3789.gets
-      return result_1, result_2, result_4, "PORT 3689 is FREE"
-    elsif result_2 = netstat_3589.gets && result_3 = netstat_3689.gets && result_4 = netstat_3789.gets
-      return result_2, result_3, result_4, "PORT 3489 is FREE"
-    elsif result_1 = netstat_3489.gets && result_3 = netstat_3689.gets && result_4 = netstat_3789.gets
-      return result_1, result_3, result_4, "PORT 3589 is FREE"
-    elsif result_1 = netstat_3489.gets && result_2 = netstat_3589.gets
-      return result_1, result_2, "PORTS 3689, 3789 are FREE"
-    elsif result_1 = netstat_3489.gets && result_3 = netstat_3689.gets
-      return result_1, result_3, "PORTS 3589, 3789 are FREE"
-    elsif result_1 = netstat_3489.gets && result_4 = netstat_3789.gets
-      return result_1, result_4, "PORTS 3589, 3689 are FREE"
-    elsif result_2 = netstat_3589.gets && result_3 = netstat_3689.gets
-      return result_2, result_3, "PORTS 3489, 3789 are FREE"
-    elsif result_2 = netstat_3589.gets && result_4 = netstat_3789.gets
-      return result_2, result_4, "PORTS 3489, 3689 are FREE"
-    elsif result_3 = netstat_3689.gets && result_4 = netstat_3789.gets
-      return result_3, result_4, "PORTS 3489, 3589 are FREE"
-    elsif result_1 = netstat_3489.gets
-      return result_1, "PORTS 3589, 3689 and 3789 are FREE"
-    elsif result_2 = netstat_3589.gets
-      return result_2, "PORTS 3489, 3689, 3789 are FREE"
-    elsif result_3 = netstat_3689.gets
-      return result_3, "PORTS 3489, 3589, 3789 are FREE"
-    elsif result_4 = netstat_3789.gets
-      return result_4, "PORTS 3489, 3589, 3689 are FREE"
-    else
-      return "PORTS 3489, 3589, 3689, 3789 are FREE"
-    end
+    netstat_3789 = IO.popen("netstat -an | grep ::3789")
+   
+    return netstat_3489, netstat_3589, netstat_3689, netstat_3789
   end
 
   def self.print_results
     result_1, result_2, result_3, result_4 = Netstat.open
-    return result_1, result_2, result_3, result_4
+    return result_1.gets, result_2.gets, result_3.gets, result_4.gets
   end
 
 end
@@ -59,6 +27,10 @@ class RunScript < WEBrick::HTTPServlet::AbstractServlet
   def do_GET(req, res)
       res['Content-Type'] = 'text/html'
       result_1, result_2, result_3, result_4 = Netstat.print_results
+      result_1 = "PORT 3489 is FREE" if result_1.nil?
+      result_2 = "PORT 3589 is FREE" if result_2.nil?
+      result_3 = "PORT 3689 is FREE" if result_3.nil?
+      result_4 = "PORT 3789 is FREE" if result_4.nil?
       res.body = "<html><title>Scanning Ports</title>
                  <h1>Results:</h1>
                  <body>
@@ -72,7 +44,7 @@ class RunScript < WEBrick::HTTPServlet::AbstractServlet
 end
 
 server = WEBrick::HTTPServer.new(:Port => 4000)
-server.mount("/run",  RunScript)
+server.mount("/netstat",  RunScript)
 
 trap("INT"){ server.shutdown }
 server.start
