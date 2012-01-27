ca31c15c8848e020d99b0a7468db313a29d613b7
Łukasz Włodarczyk <wlodi@jamaicans.com>
2012-01-27 19:07:29 +0100

    final - next refactoring

--- server.rb
+++ server.rb
@@ -2,23 +2,49 @@
 require 'webrick'
 
 class Netstat
-
-  def self.open
+ def self.open
     #check port 3489
-    netstat_3489 = IO.popen("netstat -an | grep ::3489")
+    netstat_4 = IO.popen("netstat -an | grep :0")
     #check port 3589
-    netstat_3589 = IO.popen("netstat -an | grep ::3589")
+    netstat_5 = IO.popen("netstat -an | grep :1")
     #check port 3689
-    netstat_3689 = IO.popen("netstat -an | grep ::3689")
+    netstat_6 = IO.popen("netstat -an | grep :2")
     #check port 3789
-    netstat_3789 = IO.popen("netstat -an | grep ::3789")
-   
-    return netstat_3489, netstat_3589, netstat_3689, netstat_3789
+    netstat_7 = IO.popen("netstat -an | grep :3")
+    a = Array.new
+    b = Array.new
+    c = Array.new
+    d = Array.new
+    netstat_4 = netstat_4.readlines
+    i = 0
+    netstat_4.each do |line|
+      a[i] = line
+      i = i + 1
+    end
+    netstat_5 = netstat_5.readlines
+    j = 0
+    netstat_5.each do |line|
+      b[j] = line
+      j = j + 1
+    end
+    netstat_6 = netstat_6.readlines
+    f = 0
+    netstat_6.each do |line|
+      c[f] = line
+      f = f + 1
+    end
+    netstat_7 = netstat_7.readlines
+    g = 0
+    netstat_7.each do |line|
+      d[g] = line
+      g = g + 1
+    end
+    return a, b, c, d
   end
 
   def self.print_results
+    result_1, result_2, result_3, result_4 = Array.new
     result_1, result_2, result_3, result_4 = Netstat.open
-    return result_1.gets, result_2.gets, result_3.gets, result_4.gets
   end
 
 end
@@ -26,20 +52,48 @@ end
 class RunScript < WEBrick::HTTPServlet::AbstractServlet
   def do_GET(req, res)
       res['Content-Type'] = 'text/html'
-      result_1, result_2, result_3, result_4 = Netstat.print_results
-      result_1 = "PORT 3489 is FREE" if result_1.nil?
-      result_2 = "PORT 3589 is FREE" if result_2.nil?
-      result_3 = "PORT 3689 is FREE" if result_3.nil?
-      result_4 = "PORT 3789 is FREE" if result_4.nil?
-      res.body = "<html><title>Scanning Ports</title>
-                 <h1>Results:</h1>
-                 <body>
-                 <h2>#{result_1}</h2>
-                 <h2>#{result_2}</h2>
-                 <h2>#{result_3}</h2>
-                 <h2>#{result_4}</h2>
-                 </body>
-                 </html>"
+      ree = Netstat.open
+      puts ree[0]
+      puts "--------------------------"
+      puts ree[1]
+      puts "--------------------------"
+      puts ree[2]
+      puts "-------------------------"
+      puts ree[3]
+      i = 0
+      array = Array.new
+      ree[0].each do |result|
+        result.each do |row|
+          array[i] = "<h2>#{row}</h2>"
+          i = i + 1
+        end
+      end
+      i = 0 
+      array1 = Array.new
+      ree[1].each do |result|
+        result.each do |row|
+          array1[i] = "<h2>#{row}</h2>"
+          i = i + 1
+        end
+      end
+      i = 0 
+      array2 = Array.new
+      ree[2].each do |result|
+        result.each do |row|
+          array2[i] = "<h2>#{row}</h2>"
+          i = i + 1
+        end
+      end
+      i = 0
+      array3 = Array.new
+      ree[3].each do |result|
+        result.each do |row|
+          array3[i] = "<h2>#{row}</h2>"
+          i = i + 1
+        end
+      end
+
+      res.body = "#{array}" + "#{array1}" + "#{array2}" + "#{array3}"
   end
 end
 
